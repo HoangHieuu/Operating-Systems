948b9f5e6a3260662547394ac87386fd1dc349f4
HoangHieuu <hoang.hieu.0726@gmail.com>
2026-04-03 16:56:28 +0700

    update math_logic.c

--- Lab1/math_logic.c
+++ Lab1/math_logic.c
@@ -4,12 +4,6 @@
 #include <string.h>
 #include "math_logic.h"
 
-#include <math.h>
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include "math_logic.h"
-
 int parse_operand(const char *token, double ans, double *value) {
     char *end = NULL;
 
